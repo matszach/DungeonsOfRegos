33b00f08c1acf66b535d0c1187d4da932b52e20c
matszach <lkaszubowski@gmail.com>
2021-03-14 23:35:36 +0100

    monster alerting 0.1.7

--- src/js/game/actors/Monster.js
+++ src/js/game/actors/Monster.js
@@ -15,17 +15,24 @@ class Monster extends Actor {
             const {x: px, y: py} = player;
             const diffx = this.x - px; 
             const diffy = this.y - py;
-            const dir = Root.rng.weightedPick(
-                [{x: -1, y: 0}, diffx > 0 ? 20 : 1],
-                [{x: 1, y: 0}, diffx < 0 ? 20 : 1],
-                [{x: 0, y: -1}, diffy > 0 ? 20 : 1],
-                [{x: 0, y: 1}, diffy < 0 ? 20 : 1]
-            );
-            if(this.x + dir.x === px && this.y + dir.y === py) {
-                this.attack(player);
-            } else if(this.actorCanMoveBy(dir.x, dir.y, Root.level)) {
-                this.actorMoveBy(dir.x, dir.y, Root.level);
+            if(this.alert) {
+                const dir = Root.rng.weightedPick(
+                    [{x: -1, y: 0}, diffx > 0 ? 20 : 1],
+                    [{x: 1, y: 0}, diffx < 0 ? 20 : 1],
+                    [{x: 0, y: -1}, diffy > 0 ? 20 : 1],
+                    [{x: 0, y: 1}, diffy < 0 ? 20 : 1]
+                );
+                if(this.x + dir.x === px && this.y + dir.y === py) {
+                    this.attack(player);
+                } else if(this.actorCanMoveBy(dir.x, dir.y, Root.level)) {
+                    this.actorMoveBy(dir.x, dir.y, Root.level);
+                }
+            } else if(Math.abs(diffx) < 5 && Math.abs(diffy) < 5) {
+                console.log(diffx, diffy);
+                this.alert = true;
+                // alert animation ("!") here
             }
+           
         }
     }
 
