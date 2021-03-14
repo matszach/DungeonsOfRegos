4efed80651de3dfd0e3f52bb106ad88a010f4bcd
matszach <lkaszubowski@gmail.com>
2021-03-14 20:36:06 +0100

    better temporary AI: 0.1.5

--- src/js/game/actors/Monster.js
+++ src/js/game/actors/Monster.js
@@ -9,12 +9,22 @@ class Monster extends Actor {
     }
 
     doTurn() {
-        // test monster logic FIXME
+        // TODO, logic should come from AI instance, this is a test logic
         if(Root.rng.chance(this.chanceToAct)) {
-            const x = Root.rng.pick([1, -1, 0, 0]);
-            const y = x === 0 ? Root.rng.pick([1, -1]) : 0;
-            if(this.actorCanMoveBy(x, y, Root.level)) {
-                this.actorMoveBy(x, y, Root.level);
+            const player = Root.player; 
+            const {x: px, y: py} = player;
+            const diffx = this.x - px; 
+            const diffy = this.y - py;
+            const dir = Root.rng.weightedPick(
+                [{x: -1, y: 0}, diffx > 0 ? 7 : 1],
+                [{x: 1, y: 0}, diffx < 0 ? 7 : 1],
+                [{x: 0, y: -1}, diffy > 0 ? 7 : 1],
+                [{x: 0, y: 1}, diffy < 0 ? 7 : 1]
+            );
+            if(this.x + dir.x === px && this.y + dir.y === py) {
+                this.attack(player);
+            } else if(this.actorCanMoveBy(dir.x, dir.y, Root.level)) {
+                this.actorMoveBy(dir.x, dir.y, Root.level);
             }
         }
     }
